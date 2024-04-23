9768f72a8b6266cd67dfd8b264eea0b10a7935cf
Tokar08 <ivan.tokar.git@gmail.com>
2024-04-23 22:44:24 +0300

    Rotate Left problem solved

--- ProblemSolvingExam/DataStructures.cs
+++ ProblemSolvingExam/DataStructures.cs
@@ -18,5 +18,15 @@ namespace ProblemSolvingExam
             }
             return reverseArr;
         }
+
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            for (int i = 0; i < d; i++)
+            {
+                arr.Add(arr[0]);
+                arr.RemoveAt(0);
+            }
+            return arr;
+        }
     }
 }
--- ProblemSolvingExam/Program.cs
+++ ProblemSolvingExam/Program.cs
@@ -6,7 +6,7 @@ namespace ProblemSolvingExam
     {
         static void Main()
         {
-            Console.WriteLine(string.Join(" ", DataStructures.reverseArray(new List<int> {3, 6,9,12,15 })));
+            Console.WriteLine(string.Join(" ", DataStructures.rotateLeft(1, new List<int> {3, 6, 9, 12,15 })));
             Console.ReadLine();
         }
     }
